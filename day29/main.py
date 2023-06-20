0678f2757880707b6f8e9014f271dc0419820c41
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2023-06-20 07:48:15 +0500

    Highlight color is added to entries

--- day29/main.py
+++ day29/main.py
@@ -22,17 +22,19 @@ canvas.grid(row=0, column=1)
 # website label and entry
 website_label = tk.Label(text="Website:", font=FONT, padx=30, pady=10)
 website_label.grid(row=1, column=0, sticky='w')
-website_entry = tk.Entry(width=35, font=FONT)
+website_entry = tk.Entry(width=35, font=FONT, highlightthickness=2, highlightcolor="blue")  # highlight color
+website_entry.focus()  # focus mode
 website_entry.grid(row=1, column=1, columnspan=2)
 # email label and entry
 email_label = tk.Label(text="Email/Username:", font=FONT)
 email_label.grid(row=2, column=0, sticky='w')
-email_entry = tk.Entry(width=35, font=FONT)
+email_entry = tk.Entry(width=35, font=FONT, highlightthickness=2, highlightcolor="blue")
+email_entry.insert(string="amirovsunnat@gmail.com", index=0)  # setting up default email
 email_entry.grid(row=2, column=1, columnspan=2)
 # password label and entry
 password_label = tk.Label(text="Password:", font=FONT, pady=10)
 password_label.grid(row=3, column=0)
-password_entry = tk.Entry(width=22, font=FONT)
+password_entry = tk.Entry(width=22, font=FONT, highlightthickness=2, highlightcolor="blue")
 password_entry.grid(row=3, column=1, sticky='w')
 
 # generate password button
