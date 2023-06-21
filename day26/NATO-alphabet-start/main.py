44117ed36e62d89b228440e8fbed89fb3364df5a
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2023-06-21 19:41:05 +0500

    Nato - alphabet project. Error handling is configured.

--- day26/NATO-alphabet-start/main.py
+++ day26/NATO-alphabet-start/main.py
@@ -2,7 +2,18 @@ import pandas
 
 phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_alphabet_dict = {row.letter: row.code for (index, row) in phonetic_alphabet.iterrows()}
-user_word = input("Enter a word: ").upper()
-# letter_word = {letter: word for (letter, word) in phonetic_alphabet_dict.items() for letter in user_word}
-letter_word = [phonetic_alphabet_dict[letter] for letter in user_word]
-print(letter_word)
+
+
+def nato():
+    user_word = input("Enter a word: ").upper()
+    letter_word = [phonetic_alphabet_dict[letter] for letter in user_word]
+    print(letter_word)
+
+
+while True:
+    try:
+        nato()
+        break
+    except KeyError:
+        print("Sorry only letters are allowed.")
+
